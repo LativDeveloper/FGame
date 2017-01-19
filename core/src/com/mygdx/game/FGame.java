c3f58898f66ba41c0ed673930317fb2bdbf75258
vetal <dj-ratmir@mail.ru>
2017-01-19 19:17:05 +0300

    Добавил MainMenuScreen.

--- core/src/com/mygdx/game/FGame.java
+++ core/src/com/mygdx/game/FGame.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.Screen;
 
 public class FGame extends Game {
 	private static MainGameScreen _mainGameScreen;
+	private static MainMenuScreen _mainMenuScreen;
 
 	public Screen getScreen(ScreenType screenType) {
 		switch (screenType) {
 			case MainGame:
 				return _mainGameScreen;
+			case MainMenu:
+				return _mainMenuScreen;
 			default:
 				return null;
 		}
@@ -18,6 +21,7 @@ public class FGame extends Game {
 	@Override
 	public void create() {
 		_mainGameScreen = new MainGameScreen();
+		_mainMenuScreen = new MainMenuScreen();
 
 		setScreen(_mainGameScreen);
 	}
@@ -28,6 +32,7 @@ public class FGame extends Game {
 	}
 
 	public enum ScreenType {
-		MainGame
+		MainGame,
+		MainMenu
 	}
 }
